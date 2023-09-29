b5138ed0ddecbfdde599e441be466b900b1f1582
Driconaari <asgervb@gmail.com>
2023-09-29 23:58:37 +0200

    Added items to rooms and can now pick up by using the pick (item) command. added new Enemy class. Fixed some bugs and Problems

---
+++ src/main/java/Enemy.java
@@ -0,0 +1,15 @@
+public class Enemy {
+    private String name;
+    private String description;
+    private int health;
+    private int damage;
+
+    public Enemy(String name, String description, int health, int damage) {
+        this.name = name;
+        this.description = description;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    // Getter and setter methods for properties
+}
