6585fe39ab8e27fd709d2eab52c301bc5a30e0fb
Driconaari <asgervb@gmail.com>
2023-09-29 23:55:08 +0200

    Added items to rooms and can now pick up by using the pick (item) command. added new Enemies class. Fixed some bugs and Problems

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,18 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Map {
     public Room buildMap() {
         // Create room objects and build the game map
-        Room room1 = new Room("Room 1", "A room with no distinct features, except a 'Shiny Sword' lying on the ground.");
-        Room room2 = new Room("Room 2", "A dimly lit room with a musty smell and a 'Dagger' lying on the ground, covered in blood.");
-        Room room3 = new  Room("Room 3", "A large room with a high ceiling.");
+        Room room1 = new Room("Room 1", "A room with no distinct features.");
+        Room room2 = new Room("Room 2", "A dimly lit room with a musty smell.");
+        Room room3 = new Room("Room 3", "A large room with a high ceiling.");
         Room room4 = new Room("Room 4", "A mysterious room filled with strange artifacts.");
         Room room5 = new Room("Room 5", "A hidden chamber with a single exit to the east.");
         Room room6 = new Room("Room 6", "A narrow corridor with flickering torches.");
         Room room7 = new Room("Room 7", "An underground cave with a shimmering pool.");
         Room room8 = new Room("Room 8", "A dusty library with rows of ancient books.");
         Room room9 = new Room("Room 9", "A secret chamber containing a treasure chest.");
+
+        // Create items for each room
+        Item sword1 = new Item("Shiny Sword", "A sharp and shiny sword.");
+        Item dagger1 = new Item("Dagger", "A small, sharp dagger.");
+        Item roboticEye1 = new Item("Robotic Eye", "A weird-looking robotic eye.");
+
+        Item potion = new Item("Health Potion", "A red potion that restores health.");
+        Item key = new Item("Golden Key", "A golden key with intricate engravings.");
+        Item lantern = new Item("Magic Lantern", "A lantern that emits a magical glow.");
+
+        Item gemstone = new Item("Precious Gemstone", "A glittering gemstone of great value.");
+        Item map = new Item("Ancient Map", "An old map with cryptic markings.");
+        Item scroll = new Item("Mystic Scroll", "A scroll containing mysterious incantations.");
+
+        // Add items to rooms
+        room1.addItem(sword1);
+        room2.addItem(dagger1);
+        room3.addItem(roboticEye1);
+
+        room4.addItem(potion);
+        room5.addItem(key);
+        room6.addItem(lantern);
+
+        room7.addItem(gemstone);
+        room8.addItem(map);
+        room9.addItem(scroll);
 
         // Connect rooms according to your descriptions
         room1.setNorth(room3);
@@ -47,24 +70,20 @@ public class Map {
         room9.setNorth(room6);
         room9.setEast(room5);
 
-        // Create items
-        Item sword = new Item("Shiny Sword", "A sharp and shiny sword.");
-        Item dagger = new Item("Dagger", "A small, sharp dagger.");
-        Item RoboticEye = new Item("RoboticEye", "A Wierd looking robotic eye.");
+        return room1;
 
-        // Add items to rooms
-        room1.addItem(sword);
-        room2.addItem(dagger);
-        room3.addItem(RoboticEye);
-
-        // Create the starting room
-        Room startingRoom = new Room("Starting Room", "This is where your adventure begins.");
-        startingRoom.setEast(room1); // Connect the starting room to Room 1
-        startingRoom.addItem(sword);
-        return startingRoom;
     }
 
     public Room getStartingRoom() {
-        return buildMap();
+        Room startingRoom = new Room("Starting Room", "This is where your adventure begins.");
+        startingRoom.setEast(buildMap()); // Connect the starting room to Room 1
+        startingRoom.addItem(new Item("Shiny Sword", "A sharp and shiny sword.")); // Add a new sword to the starting room
+        return startingRoom;
     }
 }
+
+
+
+
+
+
